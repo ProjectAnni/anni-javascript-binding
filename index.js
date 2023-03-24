cf69725b3bd49b78ba60b50cc49740e6b462a90f
Eridanus Sora <sora@sound.moe>
2023-03-24 10:55:49 +0800

    fix ci

--- index.js
+++ index.js
@@ -26,24 +26,24 @@ switch (platform) {
   case 'android':
     switch (arch) {
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'anni-javascript-binding.android-arm64.node'))
+        localFileExisted = existsSync(join(__dirname, '@anni-rs/anni-javascript-binding.android-arm64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./anni-javascript-binding.android-arm64.node')
+            nativeBinding = require('./@anni-rs/anni-javascript-binding.android-arm64.node')
           } else {
-            nativeBinding = require('anni-javascript-binding-android-arm64')
+            nativeBinding = require('@anni-rs/anni-javascript-binding-android-arm64')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, 'anni-javascript-binding.android-arm-eabi.node'))
+        localFileExisted = existsSync(join(__dirname, '@anni-rs/anni-javascript-binding.android-arm-eabi.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./anni-javascript-binding.android-arm-eabi.node')
+            nativeBinding = require('./@anni-rs/anni-javascript-binding.android-arm-eabi.node')
           } else {
-            nativeBinding = require('anni-javascript-binding-android-arm-eabi')
+            nativeBinding = require('@anni-rs/anni-javascript-binding-android-arm-eabi')
           }
         } catch (e) {
           loadError = e
@@ -57,13 +57,13 @@ switch (platform) {
     switch (arch) {
       case 'x64':
         localFileExisted = existsSync(
-          join(__dirname, 'anni-javascript-binding.win32-x64-msvc.node')
+          join(__dirname, '@anni-rs/anni-javascript-binding.win32-x64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./anni-javascript-binding.win32-x64-msvc.node')
+            nativeBinding = require('./@anni-rs/anni-javascript-binding.win32-x64-msvc.node')
           } else {
-            nativeBinding = require('anni-javascript-binding-win32-x64-msvc')
+            nativeBinding = require('@anni-rs/anni-javascript-binding-win32-x64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -71,13 +71,13 @@ switch (platform) {
         break
       case 'ia32':
         localFileExisted = existsSync(
-          join(__dirname, 'anni-javascript-binding.win32-ia32-msvc.node')
+          join(__dirname, '@anni-rs/anni-javascript-binding.win32-ia32-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./anni-javascript-binding.win32-ia32-msvc.node')
+            nativeBinding = require('./@anni-rs/anni-javascript-binding.win32-ia32-msvc.node')
           } else {
-            nativeBinding = require('anni-javascript-binding-win32-ia32-msvc')
+            nativeBinding = require('@anni-rs/anni-javascript-binding-win32-ia32-msvc')
           }
         } catch (e) {
           loadError = e
@@ -85,13 +85,13 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'anni-javascript-binding.win32-arm64-msvc.node')
+          join(__dirname, '@anni-rs/anni-javascript-binding.win32-arm64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./anni-javascript-binding.win32-arm64-msvc.node')
+            nativeBinding = require('./@anni-rs/anni-javascript-binding.win32-arm64-msvc.node')
           } else {
-            nativeBinding = require('anni-javascript-binding-win32-arm64-msvc')
+            nativeBinding = require('@anni-rs/anni-javascript-binding-win32-arm64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -102,23 +102,23 @@ switch (platform) {
     }
     break
   case 'darwin':
-    localFileExisted = existsSync(join(__dirname, 'anni-javascript-binding.darwin-universal.node'))
+    localFileExisted = existsSync(join(__dirname, '@anni-rs/anni-javascript-binding.darwin-universal.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./anni-javascript-binding.darwin-universal.node')
+        nativeBinding = require('./@anni-rs/anni-javascript-binding.darwin-universal.node')
       } else {
-        nativeBinding = require('anni-javascript-binding-darwin-universal')
+        nativeBinding = require('@anni-rs/anni-javascript-binding-darwin-universal')
       }
       break
     } catch {}
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, 'anni-javascript-binding.darwin-x64.node'))
+        localFileExisted = existsSync(join(__dirname, '@anni-rs/anni-javascript-binding.darwin-x64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./anni-javascript-binding.darwin-x64.node')
+            nativeBinding = require('./@anni-rs/anni-javascript-binding.darwin-x64.node')
           } else {
-            nativeBinding = require('anni-javascript-binding-darwin-x64')
+            nativeBinding = require('@anni-rs/anni-javascript-binding-darwin-x64')
           }
         } catch (e) {
           loadError = e
@@ -126,13 +126,13 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'anni-javascript-binding.darwin-arm64.node')
+          join(__dirname, '@anni-rs/anni-javascript-binding.darwin-arm64.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./anni-javascript-binding.darwin-arm64.node')
+            nativeBinding = require('./@anni-rs/anni-javascript-binding.darwin-arm64.node')
           } else {
-            nativeBinding = require('anni-javascript-binding-darwin-arm64')
+            nativeBinding = require('@anni-rs/anni-javascript-binding-darwin-arm64')
           }
         } catch (e) {
           loadError = e
@@ -146,12 +146,12 @@ switch (platform) {
     if (arch !== 'x64') {
       throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
     }
-    localFileExisted = existsSync(join(__dirname, 'anni-javascript-binding.freebsd-x64.node'))
+    localFileExisted = existsSync(join(__dirname, '@anni-rs/anni-javascript-binding.freebsd-x64.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./anni-javascript-binding.freebsd-x64.node')
+        nativeBinding = require('./@anni-rs/anni-javascript-binding.freebsd-x64.node')
       } else {
-        nativeBinding = require('anni-javascript-binding-freebsd-x64')
+        nativeBinding = require('@anni-rs/anni-javascript-binding-freebsd-x64')
       }
     } catch (e) {
       loadError = e
@@ -162,26 +162,26 @@ switch (platform) {
       case 'x64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'anni-javascript-binding.linux-x64-musl.node')
+            join(__dirname, '@anni-rs/anni-javascript-binding.linux-x64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./anni-javascript-binding.linux-x64-musl.node')
+              nativeBinding = require('./@anni-rs/anni-javascript-binding.linux-x64-musl.node')
             } else {
-              nativeBinding = require('anni-javascript-binding-linux-x64-musl')
+              nativeBinding = require('@anni-rs/anni-javascript-binding-linux-x64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'anni-javascript-binding.linux-x64-gnu.node')
+            join(__dirname, '@anni-rs/anni-javascript-binding.linux-x64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./anni-javascript-binding.linux-x64-gnu.node')
+              nativeBinding = require('./@anni-rs/anni-javascript-binding.linux-x64-gnu.node')
             } else {
-              nativeBinding = require('anni-javascript-binding-linux-x64-gnu')
+              nativeBinding = require('@anni-rs/anni-javascript-binding-linux-x64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -191,26 +191,26 @@ switch (platform) {
       case 'arm64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'anni-javascript-binding.linux-arm64-musl.node')
+            join(__dirname, '@anni-rs/anni-javascript-binding.linux-arm64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./anni-javascript-binding.linux-arm64-musl.node')
+              nativeBinding = require('./@anni-rs/anni-javascript-binding.linux-arm64-musl.node')
             } else {
-              nativeBinding = require('anni-javascript-binding-linux-arm64-musl')
+              nativeBinding = require('@anni-rs/anni-javascript-binding-linux-arm64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'anni-javascript-binding.linux-arm64-gnu.node')
+            join(__dirname, '@anni-rs/anni-javascript-binding.linux-arm64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./anni-javascript-binding.linux-arm64-gnu.node')
+              nativeBinding = require('./@anni-rs/anni-javascript-binding.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require('anni-javascript-binding-linux-arm64-gnu')
+              nativeBinding = require('@anni-rs/anni-javascript-binding-linux-arm64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -219,13 +219,13 @@ switch (platform) {
         break
       case 'arm':
         localFileExisted = existsSync(
-          join(__dirname, 'anni-javascript-binding.linux-arm-gnueabihf.node')
+          join(__dirname, '@anni-rs/anni-javascript-binding.linux-arm-gnueabihf.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./anni-javascript-binding.linux-arm-gnueabihf.node')
+            nativeBinding = require('./@anni-rs/anni-javascript-binding.linux-arm-gnueabihf.node')
           } else {
-            nativeBinding = require('anni-javascript-binding-linux-arm-gnueabihf')
+            nativeBinding = require('@anni-rs/anni-javascript-binding-linux-arm-gnueabihf')
           }
         } catch (e) {
           loadError = e
